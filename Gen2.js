53c2f8bc3681ab1df28b8e92b209469dae0ab475
MarcelTulandi <146961789+MarcelTulandi@users.noreply.github.com>
2023-12-06 18:43:44 +0800

    Update Gen2.js

---
+++ Gen2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Gen2.css';
+
+const Gen2 = (props) => {
+ return (
+    <div className="gen2">
+      <h1>{props.title}</h1>
+    </div>
+ );
+};
+
+export default Gen2;
