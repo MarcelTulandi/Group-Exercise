32c40ea0579f70b281b1f62d53cd558801d18008
MarcelTulandi <146961789+MarcelTulandi@users.noreply.github.com>
2023-12-06 18:44:54 +0800

    Update Gen4.js

---
+++ Gen4.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Gen4.css';
+
+const Gen4 = (props) => {
+ return (
+    <div className="gen4">
+      <h1>{props.title}</h1>
+    </div>
+ );
+};
+
+export default Gen4;
