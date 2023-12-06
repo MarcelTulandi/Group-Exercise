0d33dbddd42a0f312f2fc4592f3e0cd7f5eb6113
MarcelTulandi <146961789+MarcelTulandi@users.noreply.github.com>
2023-12-06 18:43:56 +0800

    Update Gen1.js

--- Gen1.js
+++ Gen1.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './Gen1.css';
 
+const Gen1 = (props) => {
+ return (
+    <div className="gen1">
+      <h1>{props.title}</h1>
+    </div>
+ );
+};
+
+export default Gen1;
