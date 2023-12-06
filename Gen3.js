38e369703931cb71652fd7b91e2ebd1ae058320e
MarcelTulandi <146961789+MarcelTulandi@users.noreply.github.com>
2023-12-06 18:44:24 +0800

    Update Gen3.js

---
+++ Gen3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Gen3.css';
+
+const Gen3 = (props) => {
+ return (
+    <div className="gen3">
+      <h1>{props.title}</h1>
+    </div>
+ );
+};
+
+export default Gen3;
